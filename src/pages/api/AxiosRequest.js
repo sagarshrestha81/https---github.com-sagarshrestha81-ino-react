83fb207d9b07e0ad9c7eab3d3cb54b63fef255c1
sagarshrestha81 <sagarcrestha81@gmail.com>
2024-09-19 19:01:54 +0545

    create student

--- src/pages/api/AxiosRequest.js
+++ src/pages/api/AxiosRequest.js
@@ -8,3 +8,22 @@ export const getData = async (params) =>{
     }
 }
 
+
+export const createData = async ({url,data}) =>{
+ 
+  const options= {
+    method:"post",
+    url:`${BASE_URL}api/${url}`,
+    headers:{
+      'Content-Type': 'application/json',
+      "Authorization": "Bearer 89704e9618f3e1a7816c2042db931eb49612a40e5ac28c037d80d4ca6ddfa442229b719e3f1bb2dda49e8ee1bbbf88cc7bbea4bb86bbe06dda4d8f715d5c0f872512b6710455a1d81b69c944d174e5af64d710b18de793f14d0ab39b582897f2622abef621d1cf6b3b4ebd4dfd47607c4b0b465ee8679b5b3658c6dcf23bd2f6",
+    },
+    data:data
+  }
+  const response = await axios.request(options);
+  if (response && response.data) {
+   return response.data.data;
+  }
+
+
+}
--- src/pages/students/CreateStudent.jsx
+++ src/pages/students/CreateStudent.jsx
@@ -1,42 +1,46 @@
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from "react";
 import axios from "axios";
+import { createData } from "./../api/AxiosRequest";
+import Swal from 'sweetalert2';
 export default function CreateStudent() {
+  const [formData, setFormData] = useState({
+    student_name: "",
+    student_class: "",
+    student_roll: "",
+  });
 
-    const [formData, setFormData ]= 
-    useState({
-        student_name:"",
-        student_class:"",
-        student_roll:""
-    })
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(formData);
 
-    const handleSubmit =(e)=>{
-        e.preventDefault();
-        console.log(formData)
-let data = JSON.stringify({
-  "data": formData
-});
 
-let config = {
-  method: 'post',
-  url: 'http://localhost:1337/api/students',
-  headers: { 
-    'Content-Type': 'application/json', 
-    'Authorization': '89704e9618f3e1a7816c2042db931eb49612a40e5ac28c037d80d4ca6ddfa442229b719e3f1bb2dda49e8ee1bbbf88cc7bbea4bb86bbe06dda4d8f715d5c0f872512b6710455a1d81b69c944d174e5af64d710b18de793f14d0ab39b582897f2622abef621d1cf6b3b4ebd4dfd47607c4b0b465ee8679b5b3658c6dcf23bd2f6'
-  },
-  data : data
-};
+    const data =
+      JSON.stringify({
+        data: formData
+      });
 
-axios.request(config)
-.then((response) => {
-  console.log(JSON.stringify(response.data));
-})
-.catch((error) => {
-  console.log(error);
-});
 
+    const response = await createData(
+      {
+        url: "students",
+        data: data
+      });
+    if (response &&
+      response.attributes &&
+      response.attributes.student_name == formData.student_name) {
+        Swal.fire({
+          icon: "success",
+          title: "Data has been saved",
+          showConfirmButton: false,
+          timer: 1500
+        });
 
     }
 
+
+
+
+  };
 
   return (
     <div className="container">
@@ -54,11 +58,11 @@ axios.request(config)
                 className="form-control"
                 id="name"
                 value={formData.student_name}
-                onChange={(e)=>{
-                    setFormData({
-                        ...formData,
-                    [e.target.name]:e.target.value
-                })
+                onChange={(e) => {
+                  setFormData({
+                    ...formData,
+                    [e.target.name]: e.target.value,
+                  });
                 }}
               />
             </div>
@@ -72,11 +76,11 @@ axios.request(config)
                 className="form-control"
                 id="roll"
                 value={formData.student_roll}
-                onChange={(e)=>{
-                    setFormData({
-                        ...formData,
-                    [e.target.name]:e.target.value
-                })
+                onChange={(e) => {
+                  setFormData({
+                    ...formData,
+                    [e.target.name]: e.target.value,
+                  });
                 }}
               />
             </div>
@@ -84,23 +88,25 @@ axios.request(config)
               <label forHtml="class" className="form-label">
                 Class
               </label>
-              <select  name="student_class" id="class"  className="form-control"
-              
-              value={formData.student_class}
-              onChange={(e)=>{
+              <select
+                name="student_class"
+                id="class"
+                className="form-control"
+                value={formData.student_class}
+                onChange={(e) => {
                   setFormData({
-                      ...formData,
-                  [e.target.name]:e.target.value
-              })
-              }}
+                    ...formData,
+                    [e.target.name]: e.target.value,
+                  });
+                }}
               >
-                <option value="" >Choose Here</option>
-                <option value="BCA" >BCA</option>
-                <option value="BIT" >BIT</option>
-                <option value="CSIT" >CSIT</option>
+                <option value="">Choose Here</option>
+                <option value="BCA">BCA</option>
+                <option value="BIT">BIT</option>
+                <option value="CSIT">CSIT</option>
               </select>
             </div>
-            
+
             <button type="submit" className="btn btn-primary">
               Submit
             </button>
